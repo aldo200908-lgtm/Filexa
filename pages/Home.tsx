9b3390bbba999835bebcd89b92536bf0a1d98ac8
aldo200908-lgtm <aldo200908@gmail.com>
2026-02-07 23:51:21 -0500

    Add Footer component to Home page

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import AppCard from '../components/AppCard'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <AppCard />
       <AppCard />
       <AppCard />
+      <Footer />
     </>
   )
 }
