1fc78979a2a17bad61531dfa95f888ea82605149
aldo200908-lgtm <aldo200908@gmail.com>
2026-02-07 23:38:37 -0500

    test: render header only

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,17 +1,9 @@
+import Header from '../components/Header'
+
 export default function Home() {
   return (
-    <div
-      style={{
-        minHeight: '100vh',
-        background: 'linear-gradient(135deg, #050b1a, #0b1d3a)',
-        color: '#00b3ff',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize: '2rem'
-      }}
-    >
-      🧩 Home de Filexa cargando correctamente
-    </div>
+    <>
+      <Header />
+    </>
   )
 }
