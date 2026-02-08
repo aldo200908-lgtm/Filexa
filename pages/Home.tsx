def1ba14e21e1dcf300d9c60d3e0f4a33afe06a8
aldo200908-lgtm <aldo200908@gmail.com>
2026-02-07 23:49:17 -0500

    Add multiple AppCard components to Home page

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,11 +1,15 @@
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import AppCard from '../components/AppCard'
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <AppCard />
+      <AppCard />
+      <AppCard />
     </>
   )
 }
